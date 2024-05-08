1a8c418580f45dcc212623a2f9235732a28999c9
prabhat kumar <prabhatkumar@prabhats-MacBook-Pro.local>
2024-05-08 16:34:16 +0530

    Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { error } from 'console';
+import { appendFileSync } from 'fs';
+import { Observable, sample } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +11,50 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   host: {ngSkipHydration: 'true'}, 
+  template:`<div>
+    <h1><title>Home</title></h1>
+  </div>`
+
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit{
   title = 'SurajMedicall';
+
+  
+  ngOnInit(){
+this.myObservable.subscribe((val:any, num:number)=>{
+  console.log(val);
+  console.log(num)
+},
+(error:any)=>{
+  alert(error.message);
+},
+()=>{
+  alert('the callback function is succesful and now can break anytime soon')
+}),
+this.myArrayObservable.subscribe((num:any)=>{
+  console.log(num)
 }
+)
+  }
+
+  myObservable = Observable.create((sample :any )=>{
+    setTimeout(()=>sample.next("A"));
+    setTimeout(()=>num.slice(2, 4))
+    setTimeout(()=> {sample.complete()}, 1000);
+    
+    // setTimeout(()=> {sample.error(new Error('Something went wrong , Plese try again soem time later')), 4000}
+    // )
+  })
+
+  myArrayObservable = Observable.create((samplee :any) =>{
+    setTimeout(()=>samplee.num);
+  })
+
+}
+
+let num= [1,2,3,4,5,6,7,8,9];
+console.log(num);
+let num1=num.slice(2);
+console.log(num1)
